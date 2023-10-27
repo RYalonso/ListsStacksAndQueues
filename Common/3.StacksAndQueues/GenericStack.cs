a13775945b294b1023133d2ab37bb614091c336c
Ivan Alonso Arevalo <ivan2003rayo@gmail.com>
2023-10-27 11:10:36 +0200

    hasta Pop() de GenericStack

--- Common/3.StacksAndQueues/GenericStack.cs
+++ Common/3.StacksAndQueues/GenericStack.cs
@@ -1,37 +1,47 @@
 
+using System.Collections.Generic;
+
 namespace Common
 {
     public class GenericStack<T> : IPushPop<T>
     {
         //TODO #1: Declare a List inside this object class to store the objects. Choose the most appropriate object class
-
+        GenericArrayList<T> List = new GenericArrayList<T>(10000000);
         public string AsString()
         {
+            string allString="";
             //TODO #2: Return the list as a string. Use the method already implemented in your list
-            return null;
+            for(int i = 0; i < List.Count(); i++)
+            {
+                allString = List.Get(i).ToString + "\n";
+            }
+            return allString;
         }
 
         public int Count()
         {
             //TODO #3: Return the number of objects
-            return 0;
+           return List.Count();
         }
 
         public void Clear()
         {
             //TODO #4: Remove all objects stored
+            List.Clear();
         }
 
         public void Push(T value)
         {
             //TODO #5: Add a new object to the list (at the end of it)
+            List.Add(value);
         }
 
         public T Pop()
         {
             //TODO #6: Remove the first object of the list and return it
-
-            return default(T);
+            List.Remove(0);
+            return List.Get(0);   
+            
         }
     }
 }
